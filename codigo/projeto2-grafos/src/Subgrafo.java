e1dab846a5fa6219918abd3d2c1790755a06366e
henriquegrissi <hgcsc2003@gmail.com>
2023-03-23 14:48:02 -0300

    Começando o método de Subgrafo

---
+++ codigo/projeto2-grafos/src/Subgrafo.java
@@ -0,0 +1,28 @@
+import static java.lang.Integer.*;
+
+public class Subgrafo{
+    private int vertices [];
+
+    
+    public Subgrafo(String v){
+
+        String[] individual = v.split(" ");
+
+        for (int i = 0; i < individual.length(); i++){
+            vertices[i] = Integer.parseInt(individual[i]);
+        }
+        
+    }
+
+    void exibeSubGrafo(){
+
+        for (int i = 0; i < vertices.length(); i++){
+            System.out.println(vertices[i]);
+        }
+
+    }
+    public void criaSubGrafo(int vertices[]){
+
+
+    }
+}
---
+++ codigo/projeto2-grafos/src/SubgrafoTest.java
@@ -0,0 +1,9 @@
+@BeforeEach
+void criar(){
+    Subgrafo = new Subgrafo("1,2,3,4");
+    
+}
+@Test
+void exibeSubGrafo(){
+
+}
