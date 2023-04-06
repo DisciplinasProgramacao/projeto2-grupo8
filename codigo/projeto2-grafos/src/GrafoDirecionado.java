c5c9a5e607664cb245d6200b91adc2535890b8b1
Laura_Melo <lauraenisiam@gmail.com>
2023-04-05 22:10:31 -0300

    #9 commitando GrafoDirecionado

---
+++ codigo/projeto2-grafos/src/GrafoDirecionado.java
@@ -0,0 +1,29 @@
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class GrafoDirecionado extends GrafoMutavel {
+
+    public GrafoDirecionado(String nome) {
+        super(nome);
+    }
+    
+    public boolean addAresta(int origem, int destino, int peso) {
+        boolean adicionou = false;
+        Vertice saida = this.existeVertice(origem);
+        Vertice chegada = this.existeVertice(destino);
+        if (saida != null && chegada != null) {
+            adicionou = (saida.addAresta(destino, peso) && chegada.addAresta(origem, peso));
+        }
+        return adicionou;
+    }
+
+    public Aresta removeAresta(int origem, int destino) {
+        return null;
+    }    
+
+}
--- codigo/projeto2-grafos/src/GrafoMutavel.java
+++ codigo/projeto2-grafos/src/GrafoMutavel.java
@@ -5,9 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class GrafoMutavel extends Grafo {
 
