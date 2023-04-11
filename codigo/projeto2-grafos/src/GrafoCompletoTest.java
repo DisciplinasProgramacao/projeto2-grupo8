de5437c18e7a93042fe5ee0319a4841e8450a1f5
henriquegrissi <hgcsc2003@gmail.com>
2023-04-11 17:54:25 -0300

    Grafo Direcionao Teste #17

--- codigo/projeto2-grafos/src/GrafoCompletoTest.java
+++ codigo/projeto2-grafos/src/GrafoCompletoTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
